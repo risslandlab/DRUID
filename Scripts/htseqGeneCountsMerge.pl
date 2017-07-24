3b7c19a43242d5a7dc931fe5b98cb8e2ca6f0ecf
risslandlab <risslandlab@gmail.com>
2017-07-24 12:15:16 -0400

    Updated htseqGeneCountsMerge.pl so that filename extraction is a little more flexible.

--- Scripts/htseqGeneCountsMerge.pl
+++ Scripts/htseqGeneCountsMerge.pl
@@ -15,8 +15,8 @@ my @types;
 my $table_ref = {};
 while ( my $file = shift @tsv_files ) {
 	say $file;
-	my ( $index ) = $file =~ /^(\w+)_R1/;
-	my ( $type ) = $file =~ /_R1_(\w+)\.tsv$/;
+	my ( $index ) = $file =~ /^(.*)_[^_]+\.tsv$/;
+	my ( $type ) = $file =~ /_([^_]+)\.tsv$/;
 	$indices{$index} = 1;
 	push @types, $type;
 	open my $file, '<', $file;
@@ -28,7 +28,6 @@ while ( my $file = shift @tsv_files ) {
 		my $gene = $values[0];
 		my $FPKM = $values[1];
 		$table_ref->{$type}{$gene}{$index} = $FPKM;
-		#$table_ref->{$gene}{$index} = $FPKM;
 	}
 	close $file;
 }
@@ -36,10 +35,8 @@ while ( my $file = shift @tsv_files ) {
 my @indices = sort { $a cmp $b } keys %indices;
 for my $type (@types){
 	open my $out, '>', "combined_${type}.txt";
-	#open my $out, '>', "combined.txt";
 	say $out join( "\t", ("Gene", "Organism", @indices ) );
 	my $type_table_ref = $table_ref->{$type};
-	#my $type_table_ref = $table_ref;
 	for my $gene ( sort( keys( %$type_table_ref ) ) ) {
 		my $geneName = substr($gene, 0, -1);
 		my $organism = substr($gene, -1);
